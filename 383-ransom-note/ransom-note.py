1a5a3fa2accbc6ee34e5586c170c5e150a8165bf
aslaxmanbabu <115299099+aslaxmanbabu@users.noreply.github.com>
2024-07-01 15:45:47 +0530

    Time: 148 ms (5.05%) | Memory: 16.9 MB (14.32%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,20 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        rn = {}
-        for i in range(len(ransomNote)):
-            if ransomNote[i] not in rn.keys():
-                rn[ransomNote[i]] = 1
-            else:
-                rn[ransomNote[i]] += 1
+        
+        for i in ransomNote:
+            if magazine.count(i) < ransomNote.count(i):
+                return False
 
-        for i in range(len(magazine)):
-            if magazine[i] not in rn.keys():
-                pass
-            else:
-                rn[magazine[i]] -= 1
-
-        if all(x <= 0 for x in rn.values()):
-            return True
-        else:
-            return False
+        return True
         
