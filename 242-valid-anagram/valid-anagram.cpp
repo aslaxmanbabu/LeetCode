c45ed1e94850144148387bc2d59b8de3beb4dc17
aslaxmanbabu <115299099+aslaxmanbabu@users.noreply.github.com>
2025-07-07 23:18:56 +0530

    Time: 7 ms (21.09%) | Memory: 9.6 MB (91.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t ? true : false;
+        map<char, int> r;
+        if(s.size() != t.size()) {
+            return false;
+        }
+
+        for(int i=0; i<s.size(); i++) {
+            r[s[i]]++;
+        }
+
+        for(int i=0; i<t.size(); i++) {
+            if(r[t[i]] <= 0) {
+                return false;
+            }
+            else {
+                r[t[i]]--;
+            }
+        }
+
+        map<char, int> :: iterator it;
+        for(it = r.begin(); it != r.end(); it++) {
+            if(it->second > 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
