713da11512f47b5a5ac5f04e670ca5f585ba206f
aslaxmanbabu <115299099+aslaxmanbabu@users.noreply.github.com>
2025-07-12 00:08:01 +0530

    Time: 5 ms (13.52%) | Memory: 28.1 MB (60.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       map<int, int> max;
-       int n = nums.size();
-       int temp=0;
-
-       if (n == 0) {
-        return 0;
-       }
-       else if(n == 1) {
-        return nums[0];
-       } 
-
-       for(int i=0; i<n; i++) {
-        max[nums[i]]++;
-       }
-
-       map<int, int> :: iterator i = max.begin();
-
-       while(i != max.end()) {
-        if (i->second > n/2) {
-            return i->first;
+        map<int, int> count;
+        for(int i=0; i<nums.size(); i++) {
+            count[nums[i]]++;
+            if(count[nums[i]] > nums.size()/2) {
+                return nums[i];
+            }
         }
-        i++;
-       } 
 
-       return 0; 
+        return 0;
     }
 };
