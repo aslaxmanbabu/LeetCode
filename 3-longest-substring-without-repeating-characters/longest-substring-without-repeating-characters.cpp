3926f1212ba0049e940b025694e6fa32cb6c6c67
aslaxmanbabu <115299099+aslaxmanbabu@users.noreply.github.com>
2025-07-26 23:20:07 +0530

    Time: 10 ms (54.31%) | Memory: 12.1 MB (45.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -7,7 +7,7 @@ public:
         int n = s.size();
 
         for (int i = 0; i < n; i++) {
-            if (m.find(s[i]) != m.end() && m[s[i]] >= left) {
+            if (m.find(s[i]) != m.end()) {
                 left = max(m[s[i]] + 1, left);
             }
             m[s[i]] = i;
